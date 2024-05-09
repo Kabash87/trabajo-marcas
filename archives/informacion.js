99b906387393873bcb7e783a91f3b96f6f62a565
soyTupapi <488755584wu@gmail.com>
2024-05-09 17:02:11 +0200

    parte script hecho 09/05

--- archives/informacion.js
+++ archives/informacion.js
@@ -1,24 +1,70 @@
-let info = {
-    name: '',
-    artist: '',
-    img: '',
-    info: ''
-},
-{
-    name: '',
-    artist: '',
-    img: '',
-    info: '' 
-},
-{
-    name: '',
-    artist: '',
-    img: '',
-    info: '' 
-},
-{
-    name: '',
-    artist: '',
-    img: '',
-    info: ''
-}
+let info = [
+    {
+      name: 'Perfect Night',
+      artist: 'Le Sserafim',
+      img: 'archives/900-x-600-featured-image-blogs-2-1-1670302216240.webp',
+      info: `Nombre:
+      LE SSERAFIM (Internacional)
+      르세라핌 (leuselapim) en Corea.
+      ル セラフィム (ru serafimu) en Japón.
+      ¿Por qué?: Su nombre está creado por el anagrama de 'I'm Fearless', e implica confianza en sí mismo y una fuerte voluntad de seguir adelante sin dejarse llevar por la mirada del mundo. LE SSERAFIM proviene originalmente del término "LES SERAPHIM", mientras que Seraphim es un ser celestial de 6 alas.
+      Origen: Corea del Sur
+      Número de integrantes: 5 chicas (2 coreanas, 2 japonesas y 1 estadounidense)
+      Número de ex-integrantes: 1 chica (coreana)
+      Debut:
+      Corea: 02 de Mayo de 2022
+      Japón: 25 de Enero de 2023
+      Nombre fanclub oficial: FEARNOT
+      ¿Por qué?: Es un homónimo para “florecer” en coreano, lleva el significado de que LE SSERAFIM marcha hacia adelante sin miedo y los fans que “no temen” amar y apoyar a LE SSERAFIM se unen para “florecer” en cada nuevo momento.
+      Color Oficial: FEARLESS BLUE (PANTONE 7453C)
+      ¿Por qué?: interpreta la temperatura más alta de una estrella. Pretende simbolizar las características libres, apasionadas y valientes de LE SSERAFIM y FEARNOT.
+      Género: K-Pop
+      Agencia:
+      Source Music (Corea del Sur)
+      Sub-Agencia: HYBE
+      EMI Records (Japón)
+      Sub-Agencia: Universal Music Japan`
+    },
+    {
+      name: 'Cupid',
+      artist: 'Fifty Fifty',
+      img: 'archives/FIFTY_FIFTY_1.webp',
+      info: `Nombre:
+      FIFTY FIFTY (Internacional)
+      피프티피프티 (pipeutipipeuti) en Corea.
+      ¿Por qué?: "FIFTY FIFTY" significa "50 vs 50", en referencia a una probabilidad "50:50" de "ideal' y "realidad" respectivamente.
+      Origen: Corea del Sur
+      Número de integrantes: 1 chica
+      Número de ex-integrantes: 3 chicas
+      Debut: 18 de noviembre de 2022
+      Nombre fanclub oficial: HUNNIES
+      ¿Por qué?: Es una abreviación del número 100 en inglés (hundred) y "honey", que se usa como un término afectivo entre los seres queridos.
+      Color oficial:
+      Agencia:
+      ATTRAKT (Corea del Sur)
+      Warner Music (Internacional)`
+    },
+  
+    {
+     name: 'i really want to stay at your house',
+     artist: 'Rosa Walton',
+     img: 'archives/Rosa Walton.jpg',
+     info: `«I Really Want to Stay at Your House» es una canción compuesta por la cantante británica Rosa Walton para el videojuego Cyberpunk 2077 estrenado en el 2020, 
+     introducida en la estación de radio ficticia 98.7 "Body Heat Radio". 
+     Esta fue lanzada a través de Lakeshore Records e incluida en el álbum de la banda sonora de dicho juego, 
+     publicado el 18 de diciembre de 2020. Más tarde, 
+     la canción se volvería viral en 2022 después de ser utilizada como tema central en el ONA ciberpunk Cyberpunk: Edgerunners de 2022 y logró ingresar al puesto 68 en el Reino Unido.
+     La pista se reutilizó en la actualización 2.1, donde los jugadores podían bailar con los personajes con los que estaban saliendo al ritmo de la canción.
+    
+     Video musical:
+     El 3 de octubre de 2022 se lanzó un vídeo musical, publicado en los canales oficiales de YouTube de Netflix y Cyberpunk 2077. El clip fue editado por Nicholas Fung y presenta una recopilación de escenas de toda la serie de anime en donde resaltan la relación romántica de los protagonistas David Martinez y Lucyna Kushinada.`
+    },
+    {
+     name: 'Breed',
+     artist: 'NIRVANA',
+     img: 'Nirvana.jpg',
+     info: `Nirvana, la icónica banda de rock estadounidense formada en Aberdeen, Washington en 1987, marcó un hito en la historia del rock. Fundada por el carismático Kurt Cobain y el talentoso Krist Novoselic, la banda pasó por varios bateristas antes de reclutar a Dave Grohl en 1990. Su éxito contribuyó a la popularización del rock alternativo, y a menudo se les identificaba como la banda líder de la Generación X.
+     A lo largo de los años 80, Nirvana se consolidó como parte de la vibrante escena grunge de Seattle y lanzó su primer álbum, "Bleach", en 1989 con el sello independiente Sub Pop. Su sonido único, caracterizado por contrastes dinámicos entre versos tranquilos y coros explosivos, se perfeccionó con el lanzamiento de su segundo álbum, "Nevermind", en 1991. Este álbum incluía el icónico sencillo "Smells Like Teen Spirit" que catapultó a la banda al estrellato internacional y marcó el final del dominio del hair metal en la música popular.
+     Con una estética punk y letras que abordaban temas de abyección y alienación social, Nirvana cautivó a una audiencia global. Tras el éxito de "Nevermind", la banda lanzó su aclamado tercer álbum, "In Utero", en 1993, consolidando aún más su estatus como uno de los actos más influyentes de la década de 1990. Sin embargo, la trágica muerte de Cobain en 1994 marcó el fin de Nirvana. A pesar de su corta carrera, su legado perdura y continúa influyendo en la cultura del rock moderno.`
+    }
+];
